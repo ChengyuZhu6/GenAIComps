c3fbcfefe49388dce273136b994aebdaddbc9046
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-08-20 11:12:51 +0800

    langchain chatglm api call
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- comps/llms/text-generation/torchserve/llm.py
+++ comps/llms/text-generation/torchserve/llm.py
@@ -6,8 +6,8 @@ import time
 import requests
 from typing import Union
 from huggingface_hub import AsyncInferenceClient
+from langchain_community.llms import ChatGLM
 from comps import (
-    GeneratedDoc,
     LLMParamsDoc,
     ServiceType,
     opea_microservices,
@@ -19,10 +19,6 @@ from comps import (
 from comps.cores.proto.api_protocol import ChatCompletionRequest, ChatCompletionResponse, ChatCompletionStreamResponse
 
 llm_endpoint = os.getenv("TORCHSERVE_ENDPOINT", "http://172.20.105.242:8080/predictions/finrag-router")
-llm = AsyncInferenceClient(
-    model=llm_endpoint,
-    timeout=600,
-)
 
 
 @register_microservice(
@@ -34,14 +30,18 @@ llm = AsyncInferenceClient(
 )
 @register_statistics(names=["router_service@llm_torchserve"])
 async def llm_generate(input: Union[LLMParamsDoc, ChatCompletionRequest]):
+    ChatGLM_llm = ChatGLM(
+        endpoint_url=llm_endpoint
+    )
     print(input)
     start = time.time()
 
     prompt = input.query
     print(prompt)
-    text_generation = await llm.post(
-        json={'data': prompt}
-    )
+    # text_generation = await llm.post(
+    #     json={'data': prompt}
+    # )
+    text_generation = ChatGLM_llm.invoke(prompt)
     # router_response = requests.post(llm_endpoint, json={'data': prompt}).json()
     # print(router_response)
     statistics_dict["router_service@llm_torchserve"].append_latency(time.time() - start, None)
